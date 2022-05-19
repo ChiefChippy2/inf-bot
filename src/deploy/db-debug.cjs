fa45a964d26b91e68a83697089b6ff5170bd3d2c
ChiefChippy2 <chiefchippy2@gmail.com>
2022-05-19 16:51:48 +0200

    feat(internal): Added DB debugging file + stricter lint

---
+++ src/deploy/db-debug.cjs
@@ -0,0 +1,27 @@
+/**
+ * Load me into node REPL!
+ * Copy and paste the following line
+const debugTools = await import('./src/deploy/db-debug.js');
+ */
+
+
+(async ()=>{
+  const db = await import('../database/index.js');
+  const fs = await import('fs/promises');
+  const {srcPath} = await import('../utils.js');
+  module.exports = {
+
+    db,
+    fs,
+    srcPath,
+    /**
+     * Backs up database
+     */
+    async backupDB() {
+      await fs.copyFile(`${srcPath}/../storage/player.db`, `${srcPath}/../storage/player-${process.pid}.db.save`);
+      console.log(`SUCCESS, backup file at ${srcPath}/../storage/player-${process.pid}.db.save`);
+    },
+
+  };
+  global.debugTools = module.exports;
+})();
