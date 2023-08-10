296796824d4b024fbc8e89bd33e205cb2895e7f6
ChiefChippy2 <chiefchippy2@gmail.com>
2023-08-11 00:24:36 +0200

    Fixed usertag

--- src/database/index.js
+++ src/database/index.js
@@ -16,6 +16,41 @@ LinkedUser.init({
     allowNull: false,
     unique: true,
   },
+  nickname: {
+    type: DataTypes.STRING,
+    defaultValue: 'main',
+  },
+  linkmethod: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  }, // could be tinyint, but it's not that important. 0 -> manual/unknown; 1 -> API friend; 2 -> OAuth
+  linkedAt: DataTypes.DATE,
+  ip: DataTypes.STRING,
+  updateDailyStatsTime: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  }, // based on UTC Hour (0 to 23)
+  statsLastUpdated: {
+    type: DataTypes.DATE,
+  },
+}, {sequelize});
+
+export class LinkedAlts extends Model {}
+LinkedAlts.init({
+  discordId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  mcUuid: {
+    type: DataTypes.UUIDV4,
+    allowNull: false,
+    unique: true,
+  },
+  nickname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   linkmethod: {
     type: DataTypes.INTEGER,
     defaultValue: 0,
